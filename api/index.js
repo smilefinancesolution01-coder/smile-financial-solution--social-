bab5d6f86031b11440e530ee3faea5d7067ecbe4
smilefinancesolution01-coder <smilefinancesolution01@gmail.com>
2026-08-06 13:16:45 +0530

    Simplify serverless handler export

--- api/index.js
+++ api/index.js
@@ -1,10 +1,4 @@
 import serverless from "serverless-http";
 import app from "../backend/app.js";
 
-// Create the serverless handler once
-const handler = serverless(app);
-
-// Export for Vercel
-export default async function (req, res) {
-  return handler(req, res);
-}
+export default serverless(app);
